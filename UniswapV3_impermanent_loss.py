25514a08765def4d4a14829451c981457e7e2ee2
dinenis <gdinenis@gmail.com>
2023-01-19 19:02:53 +0000

    Update UniswapV3_impermanent_loss.py

--- UniswapV3_impermanent_loss.py
+++ UniswapV3_impermanent_loss.py
@@ -8,19 +8,16 @@ import numpy as np
 st.title("Uniswap V3 Impermanent Loss")
 st.subheader("Input")
 
-P = st.number_input('Current Price')
+P = st.number_input('ETH Price')
 col1, col2 = st.columns(2)
 P_min = col1.number_input('Min Price')
 P_max = col2.number_input('Max Price')
 
-y = st.number_input('Token1 in Pool', value=100.00)
+y = st.number_input('USDC in Pool', value=100.00)
 L=(np.sqrt(P_min)/min(P, P_max)+1/np.sqrt(min(P, P_max)))/(1-P_min/min(P,P_max))*y                                                                               
 x=max(0,(np.sqrt(P_min)/P - 1/np.sqrt(P_max))*L+y/P)
-st.write("Token0 in Pool:")
+st.write("ETH in Pool:")
 st.text(x)
-
-st.write("L (Proxy for liquidity):")
-st.text(L)
 
 lower_bound = P_min/10000
 upper_bound = 2.1*P_max
@@ -42,37 +39,26 @@ fig.update_xaxes(title_text='Price')
 fig.update_yaxes(title_text='Impermanent Loss')
 st.plotly_chart(fig,use_container_width=True)
 
-st.subheader("Impermanent Loss Calculation")
+st.subheader("PnL Calculation")
 
-P_future= st.number_input('Future Price', value=P)
+lower_bound = P_min/10000
+upper_bound = 2.1*P_max
+step = upper_bound/10000
+prices = np.arange(P_min/1000,2*P_max+1,step)
 
-x_future = max(0,L*(min(1/np.sqrt(P_min),1/np.sqrt(P_future))-1/np.sqrt(P_max)))
-y_future = max(0,L*(min(np.sqrt(P_max),np.sqrt(P_future))-np.sqrt(P_min)))
+pnl = []
+for price in prices:
+    x_future = max(0,L*(min(1/np.sqrt(P_min),1/np.sqrt(price))-1/np.sqrt(P_max)))
+    y_future = max(0,L*(min(np.sqrt(P_max),np.sqrt(price))-np.sqrt(P_min)))
+    pnl.append(y_future + x_future * price - y)
+    
+df = pd.DataFrame()
+df["Price"] = prices
+df["PNL"] = pnl
+
+fig = px.line(df, x="Price", y="PNL", title='Impermanent loss without hedging')
+fig.update_xaxes(title_text='Price')
+fig.update_yaxes(title_text='PNL')
+fig.show()
 
 
-
-st.subheader("If In Pool")
-col1, col2 = st.columns(2)
-col1.write("Token0")
-col2.text(x_future)
-col1, col2 = st.columns(2)
-col1.write("Token1")
-col2.text(y_future)
-col1, col2 = st.columns(2)
-col1.write("Token0 Value")
-col2.text(x_future * P_future)
-col1, col2 = st.columns(2)
-col1.write("All Value")
-col2.text(y_future + x_future * P_future)
-
-st.subheader("If Not In Pool")
-col1, col2 = st.columns(2)
-col1.write("Token0 Value")
-col2.text(x*P_future)
-col1, col2 = st.columns(2)
-col1.write("All Value")
-col2.text(y+x*P_future)
- 
-col1, col2 = st.columns(2)
-col1.subheader("Loss")
-col2.subheader(-y_future - x_future * P_future + y+x*P_future)
